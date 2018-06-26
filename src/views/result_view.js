5c78841c624401af65fb704391dc889b36b362e4
Luis Tejero <luisfaridta@gmail.com>
2018-06-26 17:03:11 +0100

    Done

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -24,26 +24,12 @@ InstrumentInfoView.prototype.render = function(instrument){
   this.container.innerHTML = '';
   this.container.appendChild(infoParagraph);
   const unorderedList = document.createElement('ul');
-  this.createLi("test", unorderedList);
+  for (instrumentName of instrument.instruments ) {
+    this.createLi(instrumentName, unorderedList);
+  }
   this.container.appendChild(unorderedList);
 };
 
 
-
-// PlanetInfoView.prototype.createInfoList = function(planet) {
-//   const infoList = document.createElement('ul');
-//
-//   const liDay = this.createLi(`Day: ${planet.day} Earth days`, infoList);
-//   const liOrbit = this.createLi(`Orbit: ${planet.orbit} Earth days`, infoList);
-//   const liSurfaceArea = this.createLi(
-//     `Surface Area: ${planet.surfaceArea} Earths`,
-//     infoList
-//   );
-//   const liVolume = this.createLi(`Volume: ${planet.volume} Earths`, infoList);
-//   const liGravity = this.createLi(`Gravity: ${planet.gravity}g`, infoList);
-//   const liMoons = this.createLi(`Moons: ${planet.moons}`, infoList);
-//
-//   return infoList;
-// };
 
 module.exports = InstrumentInfoView;
